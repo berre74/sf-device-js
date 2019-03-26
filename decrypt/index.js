2b777b11d95de5d1258992d230898469ccdaa0c0
Marvin Dinneweth <marvin@mbp-van-marvin.home>
2019-03-26 20:34:21 +0100

    working decryption example

--- decrypt/index.js
+++ decrypt/index.js
@@ -1,23 +1,35 @@
 const crypto = require('@trust/webcrypto')
 var ab2str = require('arraybuffer-to-string')
+var sha256 = require('sha256');
+const hex2ascii = require('hex2ascii')
 
-//don't forget npm install
+//const secret = "ZrvcwsWtZmqunpWjl-L_RSgueq7PIwPIIPn1WhArTT8";
+const secret = "bNsaYnoLbI8ld8pED7mDKDnJsMPI0AtmmU3DZtFDc5wbRDKH+UtW5rVa7cYA0uqRw2HZKQgmarfzCSrx/HEszQ=="
+
+const s = Buffer.from("ee844b5f8321a5c3", "hex");
+const iv = Buffer.from("523a7daa64993c9b4c8fe2dda6f34277", "hex");
+const ct = Buffer.from("b2cc9b9c42a00795191599e3acc98d117e440e1afc1e5927b23cafdbf75c2ef433cb197cf76fc9c17afdbe1b58ff7394974570d18194cd71defbbf7a4b1435f62fb4ddc423e48117ee9b36a825d44b920b56a45f821816ce602eab151b2ddef92e55b45d38744daffe555ba7fe0aef20cae039e3d619a229fe4645b68ab98c989b1e35520c7d97e04df7cce8e334ad4a59f130317ef77a90451a9012620f1431e4a1249f9b507ea2d27a4a9722eab9b22efbd537c17361d45855931fa86a19d2ad66325b7275886ca81ad8f2005953a2a7f27029aff4365c0f74b520327141e14741598a0dd73bd806341cfab973a30637d7ced01025a65a454f322620c755929200ac464561dd1a76f922506d00d345ccb007b433d21f2a524a42fbc887131d576dff2479dead0b5219feb73393ae9eb852a95744563a5688c33ecb968e6d116e16545cfea2332635d7b148e3a665b31b5b57f4e12ddb3bdf8f0edc0de18f3736aa83d76fc62bcc853453cf72c21ad985bdaf07acaba821948ba9083074c92c13bfe002dd71464ee8d1410eb16b3c726525543da150d864f5fcdef558", "hex");
 
 async function decryptME() {
-    //the aes key
-    let hash = Buffer.from("6F9EC8C833F00D3F7153A818D2297119EA6E19A6770EDBACD57FD0DF4C09E423", "hex")
+    //the aes key derived from debugging in the browser
+    let hash = Buffer.from("a7c193ae14848eb903cd81c9ebf1fdb8c72b54e4c6afd513619471dc051985f7", "hex")
+
+    //do it ourselves...
+    const combo = secret + s.toString("ascii");
+    const sha = sha256(combo);
+    console.log("sha: " + sha + "/r/n");
 
     //Make cryptokey object from this key
-    let key = await crypto.subtle.importKey('raw', new Uint8Array(hash), 'AES-GCM', false, ['encrypt', 'decrypt']);
+    const key = await crypto.subtle.importKey('raw', new Uint8Array(hash), 'AES-GCM', false, ['encrypt', 'decrypt']);
 
     //decrypt
     let output = await crypto.subtle.decrypt(
         {
             name: "AES-GCM",
-            iv: Buffer.from("0bff3044d99d34a36128fd7878b67efa", "hex")
+            iv: iv
         },
         key,
-        Buffer.from("8c1f791816cbcc096f1abc200cb8cd18c50391687202c63a90c19ab74331ec9479ddb87e08968321fddaa65093eabdb1118eb308b3134259e42adafd5e5c58111c571be1f7765e0df042f67d9f51980c39cf54466f0098c4a0ba1f647ccce8faa06723007f60f7ed6b1727dcaec1aab01c3186b0ab7007389e25492df01b990fde871f1254320de91f9cf9e9159667d4c787eea38c1ec20254f88cf75a2e85d61497caf65c1c8836a03b1451a893ee45adec16825c4ea102c618763139b351b234084bef432c29f811f14cf28ac6c58f1af9757849e24467138e29402b1e0ba334ef0dce70ddee92a060230fd6d0b7c6df5d47f0703078e070ccbadd34c1701192928a97d81a2db6c4d8aded2b4242054f6be4f464ccec9797f659def87ad8d739b1fd1e2c73de1097c384bf71c1faec882bc8fe193e6bd52b8fab0245cab590484da1093263840fc75fbb324d9a1e8a411792ffd8293764a00ce6d90a8f4d308cb3d3beb017216d3ab02858a8c70faf7296d9a8afb446286a09c93a53cbe00f24240a3332b711cd232ca1c88e73b62d2e70c9cfbc4e8f392e84654ef5", "hex")
+        ct
     )
 
     //output result
